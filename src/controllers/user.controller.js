af2d36e751c3fbe01dd82be7fcf36cc96190b4ce
Jayesh Prajapat <prajapat.jayesh07@gmail.com>
2025-04-19 16:51:18 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,15 +23,21 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new ApiErr(400, "All fields are requried")
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{username}, { email }]
   })
   if(existedUser){
     throw new ApiErr(409, "User with email or username already exists")
   }
 
-  const avatarLocalPath = req.files?.avatar[0]?.path
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if(!avatarLocalPath){
     throw new ApiErr(400, "Avatar file is required")
@@ -53,7 +59,7 @@ const registerUser = asyncHandler( async (req, res) => {
     username: username.toLowerCase()
   })
 
-  const creaatedUser = await user.findById(user._id).select(
+  const creaatedUser = await User.findById(user._id).select(
     "-password -refreshToken"
   )
 
